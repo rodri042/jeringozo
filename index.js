4e8a72c5758eb248735f84c29e7cf816b68b7095
Rodrigo Alfonso <rodri042@gmail.com>
2020-01-24 01:21:57 -0300

    Adding traductor

---
+++ index.js
@@ -0,0 +1,14 @@
+const silabajs = require("./silabajs");
+
+const input = process.argv[2];
+const consonantes = /[bcdfghjklmnñpqrstvwxyz]/gim;
+const palabras = input.split(" ");
+const output = palabras.map((palabra) => {
+	const silabas = silabajs.getSilabas(palabra).silabas.map((it) => it.silaba);
+	return silabas.map((it) => {
+		const vocales = it.replace(consonantes, "");
+		const vocal = vocales[vocales.length - 1];
+		return `${it}p${vocal}`;
+	}).join("");
+}).join(" ");
+console.log(output);
