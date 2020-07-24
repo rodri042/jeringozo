dd2c669224ba27265012b568f578111514a97748
Rodrigo Alfonso <rodri042@gmail.com>
2020-07-24 07:45:10 -0300

    Creating web app

---
+++ README.md
@@ -0,0 +1,24 @@
+# Jeringozo
+
+Spanish-[Jeringoza](https://en.wikipedia.org/wiki/Jeringonza) translator!
+
+# Developing
+
+## Install
+
+```bash
+nvm use
+npm install
+```
+
+## Run the app
+
+```bash
+npm start
+```
+
+## Build
+
+```bash
+npm run build
+```
---
+++ src/jeringozo.js
@@ -0,0 +1,18 @@
+const silabajs = require("./silabajs");
+
+const input = process.argv[2];
+const consonantes = /[bcdfghjklmnñpqrstvwxyz]/gim;
+const palabras = input.split(" ");
+const output = palabras
+	.map((palabra) => {
+		const silabas = silabajs.getSilabas(palabra).silabas.map((it) => it.silaba);
+		return silabas
+			.map((it) => {
+				const vocales = it.replace(consonantes, "");
+				const vocal = vocales[vocales.length - 1];
+				return vocal ? `${it}p${vocal}` : it;
+			})
+			.join("");
+	})
+	.join(" ");
+console.log(output);
