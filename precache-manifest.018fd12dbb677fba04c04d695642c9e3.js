4ba9c9129ca3b8969b6f1b2de65fea14729588be
Rodrigo Alfonso <rodri042@gmail.com>
2020-07-25 09:16:23 -0300

    Deploy @ 25/07/2020

---
+++ precache-manifest.018fd12dbb677fba04c04d695642c9e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c67ae2503ac941cced8",
+    "url": "/jeringozo/static/js/runtime~main.a05c9e1d.js"
+  },
+  {
+    "revision": "d9b871282c47a936e386",
+    "url": "/jeringozo/static/js/main.d94eaee0.chunk.js"
+  },
+  {
+    "revision": "5358e1f20ef4483e3fb1",
+    "url": "/jeringozo/static/js/2.a6c480a1.chunk.js"
+  },
+  {
+    "revision": "d9b871282c47a936e386",
+    "url": "/jeringozo/static/css/main.5ac6947b.chunk.css"
+  },
+  {
+    "revision": "5ac4d5da886821b4db48bf2b351a9bae",
+    "url": "/jeringozo/index.html"
+  }
+];
